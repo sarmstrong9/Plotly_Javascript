c1cca75c90e16930a60c978d6a87e5f0d50378df
Armstrong <Scott.Armstrong@viasat.com>
2022-05-14 15:27:40 -0700

    bar graph in progress

--- static/js/charts.js
+++ static/js/charts.js
@@ -57,31 +57,46 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
-    // 3. Create a variable that holds the samples array. 
 
+    // 3. Create a variable that holds the samples array. 
+    var samplesArray = data.samples
     // 4. Create a variable that filters the samples for the object with the desired sample number.
+    var sampleValuesIDArray = samplesArray.filter(sampleObj => sampleObj.id == sample);
 
     //  5. Create a variable that holds the first sample in the array.
-
+    var sampleValuesID = sampleValuesIDArray[0];
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    var otuIDs = sampleValuesID.otu_ids;
+    var otuLabels = sampleValuesID.otu_labels;
+    var sampleValues = sampleValuesID.sample_values;
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
-
-    var yticks = 
+    var yticks = otuIDs.sort((a,b) =>
+        a.otuIDs - b.otuIDs).reverse();
+    console.log(yticks);
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
+    var trace = {
+        x: sampleValues.slice(0,10).reverse(),
+        y: yticks,
+        type: "bar",
+        orientation: "h",
+        text: otuIDs.slice(0,10).reverse(),
+    }
+    
+    var barData = [trace];
+
+
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+        title: "Top 10 Bacteria Cultures Found",
+        xaxis: {title: ""},
+        yaxis: {title: ""}
     };
     // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot("bar-plot", barData, barLayout,{displayModeBar: false});
   });
 }
